3df9b28f85458a80a7d9b582d57985d9a7e2d3a0
Chad Mueller <chad@websitesbeyond.com>
2020-02-02 11:04:22 -0700

    update seeding file for full name authors and real file paths

--- database/seed.js
+++ database/seed.js
@@ -1,24 +1,22 @@
 const faker = require('faker');
 const Song = require('./index');
 
-let createFakeSong = () => {
-  return new Song({
-    author: faker.name.firstName(),
-    title: faker.random.word(),
-    genre: faker.random.word(),
-    tags: faker.random.words(),
-    artwork_url: faker.system.filePath(),
-    audio_file_path: faker.system.filePath(),
-    like_count: faker.random.number(),
-    play_count: faker.random.number(),
-    repost_count: faker.random.number()
-  });
-}
-let numberOfFakeRows = 100;
-for( let i = 0; i < numberOfFakeRows; i++ ){
-  // let fakeSong = createFakeSong();
-  // fakeSong.save()
-  // .then( (addedSong)=>{
-  //   console.log(addedSong);
-  // })
+const createFakeSong = () => new Song({
+  author: faker.name.findName(),
+  title: faker.random.word(),
+  genre: faker.random.word(),
+  tags: faker.random.words(),
+  artwork_url: 'https://source.unsplash.com/350x350/?music',
+  audio_file_path: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+  like_count: faker.random.number(),
+  play_count: faker.random.number(),
+  repost_count: faker.random.number(),
+});
+const numberOfFakeRows = 100;
+for (let i = 0; i < numberOfFakeRows; i++) {
+  const fakeSong = createFakeSong();
+  fakeSong.save()
+    .then((addedSong) => {
+      console.log(addedSong);
+    });
 }
